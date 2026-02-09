f3bd4d9e548abd3f7eb9ee4c2e65c6df0ea8a57b
Sports-Dev-07 <senrayaeswaran@sportstech.de>
2026-02-09 15:19:55 +0530

    first commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -179,7 +179,7 @@ export function Hero() {
                                 {model.id === "gym-pro" && (
                                     <VideoPlayer
                                         src="/videos/gym-pro.webm"
-                                        className="scale-110 translate-y-4"
+                                        className="scale-100 -translate-y-4"
                                     />
                                 )}
                             </div>
